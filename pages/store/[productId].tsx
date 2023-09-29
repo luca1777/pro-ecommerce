bde5c4116dc8a468e55729d5efe490d67f59798b
luca1777 <luca.alexandru17@yahoo.com>
2023-09-29 20:04:26 +0300

    Create product page

---
+++ pages/store/[productId].tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+
+const ProductPage = ({params}) => { 
+    console.log(params)
+  return (
+    <Layout>123</Layout>
+  )
+}
+
+export default ProductPage
