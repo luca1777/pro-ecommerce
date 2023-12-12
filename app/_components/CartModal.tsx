d5ded13b2a89f2e7f09992919bd1cb551c116df1
luca1777 <luca.alexandru17@yahoo.com>
2023-12-12 13:37:04 +0200

    added empty cart functionality

--- app/_components/CartModal.tsx
+++ app/_components/CartModal.tsx
@@ -1,13 +1,12 @@
-"use client"
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-import { IoCloseOutline } from 'react-icons/io5';
-import Image from 'next/image';
-import { LuMinus } from "react-icons/lu";
-import { LuPlus } from "react-icons/lu";
-import { getTotalCartPrice } from '../utils/cartUtils'; 
-import PlusCartButton from './PlusCartButton';
-import MinusCartButton from './MinusCartButton';
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { IoCloseOutline } from "react-icons/io5";
+import Image from "next/image";
+import { getTotalCartPrice } from "../utils/cartUtils";
+import PlusCartButton from "./PlusCartButton";
+import MinusCartButton from "./MinusCartButton";
+import CartImage from "@/app/assets/shopping-cart-20392.png"
 
 interface Product {
   id: number;
@@ -16,7 +15,7 @@ interface Product {
   images: {
     src: string;
   }[];
-  quantity: number,
+  quantity: number;
 }
 
 const Cart = ({ isModalCartOpen, closeModal }) => {
@@ -25,23 +24,22 @@ const Cart = ({ isModalCartOpen, closeModal }) => {
 
   useEffect(() => {
     const updateCartItems = () => {
-      const storedCart = localStorage.getItem('cart');
+      const storedCart = localStorage.getItem("cart");
       const cart = storedCart ? JSON.parse(storedCart) : [];
       setCartItems(cart);
       settotalPrice(getTotalCartPrice());
-  };
+    };
 
-    window.addEventListener('cartUpdated', updateCartItems);
+    window.addEventListener("cartUpdated", updateCartItems);
 
     updateCartItems();
 
     return () => {
-        window.removeEventListener('cartUpdated', updateCartItems);
+      window.removeEventListener("cartUpdated", updateCartItems);
     };
-
   }, []);
 
-  const modalPosition = !isModalCartOpen ? 'translate-x-0' : 'translate-x-full';
+  const modalPosition = !isModalCartOpen ? "translate-x-0" : "translate-x-full";
 
   return (
     <div>
@@ -56,67 +54,76 @@ const Cart = ({ isModalCartOpen, closeModal }) => {
               </div>
             </button>
           </div>
-          <div className="flex h-full flex-col justify-between overflow-hidden p-1">
-            <ul className="flex-grow overflow-auto py-4">
-              {cartItems.map((item) => (
-                <li
-                  className="flex w-full flex-col border-b border-neutral-300"
-                  key={item.id}
-                >
-                  <div className="relative flex w-full flex-row justify-between px-1 py-4">
-                    <div className="absolute z-40 -mt-2 ml-[55px]">
-                      <button className="ease flex h-[17px] w-[17px] items-center justify-center rounded-full bg-neutral-500 transition-all duration-200">
-                        <IoCloseOutline className="hover:text-accent-3 mx-[1px] h-4 w-4 text-white" />
-                      </button>
-                    </div>
-                    <Link className="z-30 flex flex-row space-x-4" href="/">
-                      <div className="relative h-16 w-16 cursor-pointer overflow-hidden rounded-md border border-neutral-300 bg-neutral-300">
-                        <Image
-                          width="64"
-                          height="64"
-                          alt="Product img"
-                          className="h-full w-full object-cover"
-                          src={item.images[0].src}
-                        />
-                      </div>
-                      <div className="flex flex-1 flex-col text-base">
-                        <span className="font-medium">{item.name}</span>
-                      </div>
-                    </Link>
-                    <div className="flex h-16 flex-col justify-between">
-                      <p className="flex justify-end space-y-2 text-right text-sm font-medium">
-                        {item.price}.00
-                        <span className="ml-1 inline font-medium">RON</span>
-                      </p>
-                      <div className="ml-auto flex h-9 flex-row items-center rounded-full border border-neutral-200">
-                        <MinusCartButton product={item} />
-                        <p className="w-6 text-center">
-                          <span className="w-full text-sm">
-                            {item.quantity}
-                          </span>
-                        </p>
-                        <PlusCartButton product={item} />
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-            <div className="py-4 text-sm text-neutral-500">
-              <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1">
-                <p>Shipping</p>
-                <p className="text-right">Calculated at checkout</p>
-              </div>
-              <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1">
-                <p>Total</p>
-                <p className="text-right text-base text-black">
-                  {totalPrice.toFixed(2)}
-                  <span className="ml-1 inline">RON</span>
-                </p>
-              </div>
+          {cartItems.length === 0 ? (
+            <div className="mt-20 flex flex-col items-center justify-center overflow-hidden py-10">
+              <Image height={50} src={CartImage} alt="Cart Img" />
+              <p className="text-2xl mt-6 font-bold">Your Cart is Empty</p>
             </div>
-            <button className='block w-full rounded-full bg-blue-600 p-3 text-center text-sm font-medium text-white opacity-90 hover:opacity-100'>Proceed to Checkout</button>
-          </div>
+          ) : (
+            <div className="flex h-full flex-col justify-between overflow-hidden p-1">
+              <ul className="flex-grow overflow-auto py-4">
+                {cartItems.map((item) => (
+                  <li
+                    className="flex w-full flex-col border-b border-neutral-300"
+                    key={item.id}
+                  >
+                    <div className="relative flex w-full flex-row justify-between px-1 py-4">
+                      <div className="absolute z-40 -mt-2 ml-[55px]">
+                        <button className="ease flex h-[17px] w-[17px] items-center justify-center rounded-full bg-neutral-500 transition-all duration-200">
+                          <IoCloseOutline className="hover:text-accent-3 mx-[1px] h-4 w-4 text-white" />
+                        </button>
+                      </div>
+                      <Link className="z-30 flex flex-row space-x-4" href="/">
+                        <div className="relative h-16 w-16 cursor-pointer overflow-hidden rounded-md border border-neutral-300 bg-neutral-300">
+                          <Image
+                            width="64"
+                            height="64"
+                            alt="Product img"
+                            className="h-full w-full object-cover"
+                            src={item.images[0].src}
+                          />
+                        </div>
+                        <div className="flex flex-1 flex-col text-base">
+                          <span className="font-medium">{item.name}</span>
+                        </div>
+                      </Link>
+                      <div className="flex h-16 flex-col justify-between">
+                        <p className="flex justify-end space-y-2 text-right text-sm font-medium">
+                          {item.price}.00
+                          <span className="ml-1 inline font-medium">RON</span>
+                        </p>
+                        <div className="ml-auto flex h-9 flex-row items-center rounded-full border border-neutral-200">
+                          <MinusCartButton product={item} />
+                          <p className="w-6 text-center">
+                            <span className="w-full text-sm">
+                              {item.quantity}
+                            </span>
+                          </p>
+                          <PlusCartButton product={item} />
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <div className="py-4 text-sm text-neutral-500">
+                <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1">
+                  <p>Shipping</p>
+                  <p className="text-right">Calculated at checkout</p>
+                </div>
+                <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1">
+                  <p>Total</p>
+                  <p className="text-right text-base text-black">
+                    {totalPrice.toFixed(2)}
+                    <span className="ml-1 inline">RON</span>
+                  </p>
+                </div>
+              </div>
+              <button className="block w-full rounded-full bg-blue-600 p-3 text-center text-sm font-medium text-white opacity-90 hover:opacity-100">
+                Proceed to Checkout
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
